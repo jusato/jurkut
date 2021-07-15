7a2dda7d87ed715efde2d69b6a42e227b2b8823e
Júlia Sato <juliasato13@gmail.com>
2021-07-15 14:44:32 -0300

    meu nome

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}> 
           <Box> 
             <h1 className="title">
-              Bem vindo
+              Júlia Sato
             </h1>
 
             <OrkutNostalgicIconSet />
